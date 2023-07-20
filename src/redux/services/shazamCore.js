d0d34b2bbaa3bb04c5a0dcc070b1d92f05264741
Ibiyemi Oluwamoduoe <ibiyemideborah@gmail.com>
2023-07-20 14:32:00 +0100

    Music App

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-key",
+        "KJwZZIJSFimshuivMSVGaiYzkRomp15f2vkjsnK4bKzuUzVLzA"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+export const { useGetTopChartsQuery } = shazamCoreApi;
